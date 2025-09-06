693499028d3a70d0c5a43f5387c797a96c3645b4
EileenMendoza18 <eileenjml4816@gmail.com>
2025-09-06 18:10:24 -0500

    Se realiza la calculadora con valores basicos

---
+++ Ejercicios/Calculadora.js
@@ -0,0 +1,71 @@
+console.log("\nOpciones: \n1-Sumar \n2-Restar \n3-Multiplicar \n4-Dividir \n5-Salir");
+
+const readline = require("readline");
+const opciones = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const menu = () => {
+
+    opciones.question("\nIngrese la opcion que desea: ", (num_opc)=>{
+        const opcion=Number(num_opc);
+
+        switch (opcion) {
+            case 1:
+            case 2:
+            case 3:
+            case 4: 
+                opciones.question("\nIngrese el primer numero: ", (valor1) => {
+                const numero1 = Number(valor1);
+
+                if (isNaN(numero1)) {
+                    console.log("\nNo es un numero valido");
+                    return menu(); 
+                }
+
+                opciones.question("\nIngrese el segundo numero: ", (valor2) => {
+                    const numero2 = Number(valor2);
+
+                    if (isNaN(numero2)) {
+                        console.log("\nNo es un numero valido");
+                        return menu();
+                    }
+                    else {
+                        switch (opcion) {
+                            case 1:
+                                console.log(`El resultado de la suma es: ${numero1 + numero2}`);
+                                break;
+                            case 2:
+                                console.log(`El resultado de la resta es: ${numero1 - numero2}`);
+                                break;
+                            case 3:
+                                console.log(`El resultado de la multiplicacion es: ${numero1 * numero2}`);
+                                break;
+                            case 4:
+                                if (numero2 === 0) {
+                                    console.log("No se puede dividir entre 0");
+                                } else {
+                                    console.log(`El resultado de la división es: ${numero1 / numero2}`);
+                                }
+                                break;
+                            }
+                        }   
+
+                    menu();
+                });
+            });
+            break
+            case 5:
+                console.log("\nSaliendo de la calculadora ...");
+                opciones.close();
+                return;
+            default:
+                console.log("\nLa opcion ingresada no es valida");
+                break;
+        }
+    });
+};
+
+// Inicia el menú
+menu();
