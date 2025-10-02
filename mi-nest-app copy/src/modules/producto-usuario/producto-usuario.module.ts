65a6aae4c41dffbd51bb82c28efd174c6d2012b7
EileenMendoza18 <eileenjml4816@gmail.com>
2025-10-01 21:08:55 -0500

    Limpieza de codigo de modulo producto-usuario

--- mi-nest-app copy/src/modules/producto-usuario/producto-usuario.module.ts
+++ mi-nest-app copy/src/modules/producto-usuario/producto-usuario.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ProductoUsuarioController } from './producto-usuario.controller';
 import { ProductoUsuarioService } from './producto-usuario.service';
-import { UsersModule } from '../users/users.module';     // Asegúrate de que exista y exporte UsersService
-import { ProductsModule } from '../products/products.module'; // Asegúrate de que exista y exporte ProductsService
+import { UsersModule } from '../users/users.module';     
+import { ProductsModule } from '../products/products.module'; 
 
 @Module({
   imports: [UsersModule, ProductsModule],
