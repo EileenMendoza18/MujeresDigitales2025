020c1d18ef73586caf7fc97cd26e9ff1c9ea7817
EileenMendoza18 <eileenjml4816@gmail.com>
2025-10-04 20:20:00 -0500

    enlazar base de datos con proyecto

--- mi-nest-app copy/src/app.module.ts
+++ mi-nest-app copy/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import {ConfigModule} from '@nestjs/config';
+import {ConfigModule, ConfigService} from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
@@ -8,10 +8,36 @@ import { ProductsModule } from './modules/products/products.module';
 import { ProductsController } from './modules/products/products.controller';
 import { ProductsService } from './modules/products/products.service';
 import { AuthModule } from './modules/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal:true}),UsersModule, ProductsModule, ProductoUsuarioModule, ProductsModule, AuthModule],
-  controllers: [AppController, ProductsController, ProductsController],
-  providers: [AppService, ProductsService],
+  imports: [
+    ConfigModule.forRoot({isGlobal:true}),
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory:(config:ConfigService)=>({
+        type:'mysql',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+    }),
+    UsersModule, 
+    ProductsModule, 
+    ProductoUsuarioModule, 
+    ProductsModule, AuthModule],
+  controllers: [
+    AppController,
+    ProductsController, 
+    ProductsController],
+  providers: [
+    AppService, 
+    ProductsService],
 })
 export class AppModule {}
