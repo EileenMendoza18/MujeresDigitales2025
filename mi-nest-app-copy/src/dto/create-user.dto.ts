b94b2f36338b03a3f8c33074f6bc3b14dc7c8282
EileenMendoza18 <eileenjml4816@gmail.com>
2025-10-08 21:36:49 -0500

    Inicio documentacion al respecto del modulo de usuario

--- mi-nest-app-copy/src/dto/create-user.dto.ts
+++ mi-nest-app-copy/src/dto/create-user.dto.ts
@@ -1,16 +1,35 @@
 import { IsEmail, IsInt, IsNotEmpty, IsOptional, Length, Max, Min} from "class-validator";
 
 export class CreateUserDTO{
+
+    // NOMBRE DEL USUARIO
+    // - Campo obligatorio
+    // - No puede estar vacio
     @IsNotEmpty({message: 'El nombre no puede ir vacio'})
     name:string; 
     
+    // CORREO ELECTRÓNICO DEL USUARIO
+    // - Campo obligatorio
+    // - No puede ir vacio
+    // - Debe tener formato de correo valido 
     @IsNotEmpty({message: 'El email no puede ir vacio'})
     @IsEmail()
     email:string; 
+
+    // CONTRASEÑA DEL USUARIO
+    // - Campo obligatorio
+    // - No puede ir vacio
+    // - Debe tener entre 6 y 15 caracteres
     
     @IsNotEmpty({message: 'La contraseña no puede ir vacia'})
     @Length(6,15, {message: 'La contraseña debe tener un minimo de 6 y maximo de 15 caracteres'})
     password: string;
+
+    // EDAD DEL USUARIO (opcional)
+    // - Campo opcional
+    // - Debe ser un numero entero (sin decimales)
+    // - Edad minima: 18 años
+    // - Edad maxima: 100 años
 
     @IsOptional()
     @IsInt({message: 'La edad debe ser un valor numerico'})
--- mi-nest-app-copy/src/migrations/1759757005744-SeedUsers.ts
+++ mi-nest-app-copy/src/migrations/1759757005744-SeedUsers.ts
@@ -1,7 +1,12 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
+// Esta migracion se encarga de insertar datos iniciales (semilla o seed)
+// en la tabla user, su objetivo es precargar usuarios base que 
+// puedan utilizarse para pruebas o configuraciones iniciales del sistema
 export class SeedUsers1759757005744 implements MigrationInterface {
 
+    // Este metodo se ejecuta cuando se aplica la migracion
+    // Inserta dos registros en la tabla user con datos de ejemplo.
     public async up(queryRunner: QueryRunner): Promise<void> {
     
         await queryRunner.query(`
@@ -14,6 +19,10 @@ export class SeedUsers1759757005744 implements MigrationInterface {
         `)
 
     }
+
+    // Este metodo revierte los cambios realizados por el metodo up()
+    // En este caso, elimina los registros insertados previamente 
+    // usando el correo electronico como criterio de identificacion
 
     public async down(queryRunner: QueryRunner): Promise<void> {
     
